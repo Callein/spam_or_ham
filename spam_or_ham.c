008e2d53451090f8e70720c9b2c89d9fa3e2a052
Somy-John <john621145@gmail.com>
2021-11-29 21:53:57 +0900

    테스트 메일 한 통씩  자름

--- spam_or_ham.c
+++ spam_or_ham.c
@@ -10,21 +10,25 @@ struct Word
    double q;
 };
 
-struct Retset{
+struct Retset
+{
    char num[10];
    double r;
-}
+};
 
 void readTrainFile(char *filename, struct Word *words, int *count);
+void readTestFile(char *filename, struct Word *spamWords, struct Retset *retset, char type);
 void calcProbability(struct Word *spamWords, struct Word *hamWords, int spamCount, int hamCount);
 
 int main(int argc, char *argv[])
 {
+
    char *trainSpamFile = "datasets/train/dataset_spam_train100.csv";
    char *trainHamFile = "datasets/train/dataset_ham_train100.csv";
    char *testSpamFile = "datasets/test/dataset_spam_test20.csv";
    char *testHamFile = "datasets/test/dataset_ham_test20.csv";
    struct Word spamWords[10000] = {0};
+
    struct Word hamWords[10000] = {0};
    struct Retset spamRetset[20] = {0};
    struct Retset hamRetset[20] = {0};
@@ -36,14 +40,20 @@ int main(int argc, char *argv[])
 
    calcProbability(spamWords, hamWords, spamCount, hamCount);
 
-   printf("%d\n", hamCount);
-   printf("%d", spamCount);
+   readTestFile(testSpamFile, spamWords, spamRetset, 's');
+   readTestFile(testHamFile, spamWords, hamRetset, 'h');
+
+   // printf("%s / %f\n", spamWords[0].word, spamWords[0].p);
+   // printf("%s / %f\n", spamWords[1].word, spamWords[1].p);
+   // printf("%d\n", hamCount);
+   // printf("%d", spamCount);
 
    return 0;
 }
 
 void calcProbability(struct Word *spamWords, struct Word *hamWords, int spamCount, int hamCount)
 {
+
    for (int i = 0; i <= spamCount; i++)
    {
       int hc = 1; // 해당 단어가 햄에 한번도 나오지 않은 경우 확률이 1이 되므로, 초기값을 1로 설정
@@ -60,8 +70,103 @@ void calcProbability(struct Word *spamWords, struct Word *hamWords, int spamCoun
    }
 }
 
-void readTestFile(char *filename, struct Word *spamWords, struct Retset *retset){
-   
+void readTestFile(char *filename, struct Word *spamWords, struct Retset *retset, char type)
+{
+   struct Word words[10000] = {0};
+   int count = 0;
+   char tok[13];
+
+   if (type == 's')
+   {
+      sprintf(tok, "sspamsubject");
+   }
+   else
+   {
+      sprintf(tok, "hhamsubject");
+   }
+
+   char str_tmp[1024];
+   int n = 0;
+
+   FILE *pFile = NULL;
+
+   pFile = fopen(filename, "r");
+   if (pFile != NULL)
+   {
+      fgets(str_tmp, 1024, pFile);
+      int c1 = 0;
+      int c2 = 0;
+      while (!feof(pFile))
+      {
+         char temp[1024] = {
+             0,
+         };
+         int x = 0;
+         fgets(str_tmp, 1024, pFile);
+
+         for (int i = 0; i < strlen(str_tmp); i++)
+         {
+            if (isalpha(str_tmp[i]))
+            {
+               if (isupper(str_tmp[i]))
+               {
+                  temp[x++] = str_tmp[i] + 32;
+               }
+               else
+               {
+                  temp[x++] = str_tmp[i];
+               }
+            }
+            else if (str_tmp[i] == ' ')
+            {
+               temp[x++] = str_tmp[i];
+            }
+         }
+
+         char *ptr = strtok(temp, " ");
+         while (ptr != NULL)
+         {
+            // printf("%s", ptr);
+            if (count != 0 && !strcmp(tok, ptr))
+            {
+               printf(" 요놈 %d\n", count);
+               // if (words[0].count == 0)
+               // {
+               //    printf("잡았다 요놈\n");
+               //    struct Word dummy[10000] = {0};
+               //    memcpy(words, dummy, sizeof(dummy));
+               // }
+               n = 0;
+               ptr = strtok(NULL, " ");
+               // c1++;
+            }
+            else
+            {
+               // printf("%s\n", words[0].word);
+               for (int i = 0; i <= count; i++)
+               {
+                  if (!strcmp((words[i]).word, ptr))
+                  {
+                     (words[i]).count++;
+                     n = 1;
+                  }
+               }
+               if (n != 1)
+               {
+                  strcpy((words[count]).word, ptr);
+                  (words[count]).count++;
+                  count++;
+               }
+               n = 0;
+               ptr = strtok(NULL, " ");
+               // c2++;
+            }
+         }
+      }
+      // printf("%d , %d", c1, c2);
+   }
+
+   fclose(pFile);
 }
 
 void readTrainFile(char *filename, struct Word *words, int *count)
@@ -74,6 +179,7 @@ void readTrainFile(char *filename, struct Word *words, int *count)
    pFile = fopen(filename, "r");
    if (pFile != NULL)
    {
+      fgets(str_tmp, 1024, pFile);
       while (!feof(pFile))
       {
          char temp[1024] = {
@@ -84,7 +190,18 @@ void readTrainFile(char *filename, struct Word *words, int *count)
 
          for (int i = 0; i < strlen(str_tmp); i++)
          {
-            if (isalpha(str_tmp[i]) || str_tmp[i] == ' ')
+            if (isalpha(str_tmp[i]))
+            {
+               if (isupper(str_tmp[i]))
+               {
+                  temp[x++] = str_tmp[i] + 32;
+               }
+               else
+               {
+                  temp[x++] = str_tmp[i];
+               }
+            }
+            else if (str_tmp[i] == ' ')
             {
                temp[x++] = str_tmp[i];
             }
@@ -93,22 +210,30 @@ void readTrainFile(char *filename, struct Word *words, int *count)
          char *ptr = strtok(temp, " ");
          while (ptr != NULL)
          {
-            for (int i = 0; i <= (*count); i++)
+            if (!strcmp("spamsubject", ptr) || !strcmp("hamsubject", ptr))
             {
-               if (!strcmp((words[i]).word, ptr))
+               n = 0;
+               ptr = strtok(NULL, " ");
+            }
+            else
+            {
+               for (int i = 0; i <= (*count); i++)
                {
-                  (words[i]).count++;
-                  n = 1;
+                  if (!strcmp((words[i]).word, ptr))
+                  {
+                     (words[i]).count++;
+                     n = 1;
+                  }
                }
+               if (n != 1)
+               {
+                  strcpy((words[(*count)]).word, ptr);
+                  (words[(*count)]).count++;
+                  (*count)++;
+               }
+               n = 0;
+               ptr = strtok(NULL, " ");
             }
-            if (n != 1)
-            {
-               strcpy((words[(*count)]).word, ptr);
-               (words[(*count)]).count++;
-               (*count)++;
-            }
-            n = 0;
-            ptr = strtok(NULL, " ");
          }
       }
    }
